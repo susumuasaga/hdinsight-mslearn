ed34ee0ee8cb44bea1c788bfcbffa764152441da
Susumu Asaga <susumu.asaga@gmail.com>
2020-09-07 21:31:44 -0300

    Update producer simulator

--- Spark Structured Streaming/python-producer-simulator-template.py
+++ Spark Structured Streaming/python-producer-simulator-template.py
@@ -1,87 +1,92 @@
-# Replace the TODOs with parameters specific to your setup
-
-# imports
-from time import sleep
-from json import dumps
+"""
+Produce simulated stock quotes as a Kafka Producer
+"""
+import argparse
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
 
-import sys
-import requests
 import json
 import random
 import time
 
-## Configuration section
-iextoken = '' #An iexToken to use if you want to connect with the iex for stock quotes
-stockList = ['msft', 'ba', 'jnj', 'f', 'tsla', 'bac', 'ge', 'mmm', 'intc', 'wmt']
-# Use this dictionary to generate random prices for each stock based on mean and standard deviation
-stockMeanPrices = 	{'msft':{"mean":152, "stddev":5}, 'ba':{"mean":345, "stddev":10}, 'jnj':{"mean":141, "stddev":10}, 'f':{"mean":9, "stddev":1}, 'tsla':{"mean":330, "stddev":10}, \
-					'bac':{"mean":34, "stddev":1}, 'ge':{"mean":11, "stddev":1}, 'mmm':{"mean":168, "stddev":5}, 'intc':{"mean":55, "stddev":2}, 'wmt':{"mean":120, "stddev":3}}
-defaultLoops = 1000
-kafkaBrokers = [
-	'wn0-kafkas.cdpy143v1neujejbg4fhxrukga.bx.internal.cloudapp.net'
-	':9092'
+iextoken = ""
+stock_list = [
+	"msft", "ba", "jnj", "f", "tsla", "bac", "ge", "mmm", "intc", "wmt"
 ]
-kafkaTopic = 'stockVals'
-simulator = True
+stock_stats = {
+	"msft": {"mean": 152, "stddev": 5}, "ba": {"mean": 345, "stddev": 10},
+	"jnj": {"mean": 141, "stddev": 10}, "f": {"mean": 9, "stddev": 1},
+	"tsla": {"mean": 330, "stddev": 10}, "bac": {"mean": 34, "stddev": 1},
+	"ge": {"mean": 11, "stddev": 1}, "mmm": {"mean": 168, "stddev": 5},
+	"intc": {"mean": 55, "stddev": 2}, "wmt": {"mean": 120, "stddev": 3},
+}
+kafka_brokers = [
+	"wn0-kafkas.cdpy143v1neujejbg4fhxrukga.bx.internal.cloudapp.net:9092",
+]
+kafka_topic = "stockVals"
 
-## Read command line arguments
-try:
-	loops = int(sys.argv[1])
-	print "Number of loops is ", loops
-except:
-	loops = defaultLoops
-	print "No loops argument provided. Default loops are ", loops
+parser = argparse.ArgumentParser()
+parser.add_argument("loops", type=int, default=1000)
+args = parser.parse_args()
+print(f"Number of loops is {args.loops}")
 
-## notify if simulator is on
-if simulator:
-	print ("Running in simulated mode")
+print("Running in simulated mode")
 
-## Construct parameters for the REST Request
-requestParams = {'token':iextoken, 'symbols':",".join(stockList)}
+request_params = {"token": iextoken, "symbols": ",".join(stock_list)}
 
-# Configure Producer
-producer = KafkaProducer(bootstrap_servers=kafkaBrokers,key_serializer=lambda k: k.encode('ascii','ignore'),value_serializer=lambda x: dumps(x).encode('utf-8'))
+producer = KafkaProducer(
+	bootstrap_servers=kafka_brokers,
+	key_serializer=lambda k: k.encode("ascii", "ignore"),
+	value_serializer=lambda x: json.dumps(x).encode("utf-8")
+)
 
-#### Main definition
+
 def main():
-	# Send messages
-	for x in range(loops):
-		## Make a rest call from Python to get streaming data
-		if simulator:
-			responseList = simulatedResponse()
-		else:
-			responseList = requests.get(url=getStockApi, params=requestParams).json()
+	"""
+	Main routine produce `args.loops` simulated list of stock prices
+	to `kafka_topic`, one per second.
+	"""
+	for x in range(args.loops):
+		response_list = simulated_response()
 
-		print(json.dumps(responseList, indent=2))
-		for stock in responseList:
-			future = producer.send(kafkaTopic, key=stock["symbol"], value=stock)
-		   	response = future.get(timeout=10)
-			print (response.topic)
-		   	print (response.partition)
-		   	print (response.offset)
-		sleep(1)
+		print(json.dumps(response_list, indent=2))
+		for stock in response_list:
+			future = producer.send(
+				kafka_topic, key=stock["symbol"], value=stock,
+			)
+			response = future.get(timeout=10)
+			print(response.topic)
+			print(response.partition)
+			print(response.offset)
+		time.sleep(1)
 
 
-## Define simulator method: returns list with artificial stock data 
-def simulatedResponse():
-	# Empty json request
-	responseList = []
-	# Create a record for each stock and add it to the json request
-	for stock in stockList:
-		stockRecord = {
-			"symbol" : stock.upper(),
-			"time" : currentMilliTime(),
-			"price" : round(random.gauss(stockMeanPrices[stock]["mean"], stockMeanPrices[stock]["stddev"]),3),
-			"size" : random.randint(1,500)
+def simulated_response():
+	"""
+	Define simulator method: returns list with artificial stock data
+	"""
+	response_list = []
+	for stock in stock_list:
+		stock_record = {
+			"symbol": stock.upper(),
+			"time": current_milli_time(),
+			"price": round(
+				random.gauss(
+					stock_stats[stock]["mean"],
+					stock_stats[stock]["stddev"]
+				), 3
+			),
+			"size": random.randint(1, 500),
 		}
-		responseList.append(stockRecord)
+		response_list.append(stock_record)
 
-	return responseList
+	return response_list
 
-def currentMilliTime():
-	return int(round(time.time() * 1000))
 
-#### Main execution
+def current_milli_time():
+	"""
+	returns time in milliseconds
+	"""
+	return round(time.time() * 1000)
+
+
 main()
